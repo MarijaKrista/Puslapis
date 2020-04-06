06c386c0bf8cbfff450230a9e0e396983299c1d0
MarijaKrista <58476096+MarijaKrista@users.noreply.github.com>
2020-04-06 19:22:36 +0300

    Add files via upload

---
+++ Background.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Background.css';
+import foto from './foto.jpg';
+
+
+function Foto() {
+    return <img src={foto} alt="Foto" />;
+  }
+
+  export default Foto;
