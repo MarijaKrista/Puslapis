a9290b4c43a09d9db09a2acfa7eb97b768d5ef00
MarijaKrista <58476096+MarijaKrista@users.noreply.github.com>
2020-04-06 19:26:37 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Navigation from './Navigation/Navigation';
+// import Background from './Background/Background';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <Navigation />
+      {/* <Background /> */}
+      </header>
+    </div>
+  );
+}
+
+
+export default App;
